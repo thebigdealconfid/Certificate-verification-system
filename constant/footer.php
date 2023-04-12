37f03f7ebf151774cf298b27c339ad284d938a85
thebigdealconfid <93054334+thebigdealconfid@users.noreply.github.com>
2023-04-12 21:47:53 +0300

    First commit
    First Date: 12-04-2023

---
+++ constant/footer.php
@@ -0,0 +1,56 @@
+<footer id="footer" class="section">
+	<div class="container">
+
+		<div class="row">
+
+			<!-- footer logo -->
+			<div class="col-md-6">
+				<div class="footer-logo">
+					<a class="logo" href="./" style="font-size: 150%;">
+						<!-- <img src="./img/logo.png" alt="logo"> -->
+						Jtech cert
+					</a>
+				</div>
+			</div>
+			<!-- footer logo -->
+
+			<!-- footer nav -->
+			<div class="col-md-6">
+				<ul class="footer-nav">
+				  <li><i class="fa fa-envelope"></i>josephchafutwa@gmail.com</li>
+				  <li><i class="fa fa-phone"></i>+254745150324</li>
+				</ul>
+			</div>
+			<!-- /footer nav -->
+		</div>
+
+		<div id="bottom-footer" class="row">
+
+			<!-- social -->
+			<div class="col-md-4 col-md-push-8">
+				<ul class="footer-social">
+					<li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+				</ul>
+			</div>
+			<!-- /social -->
+
+			<!-- copyright -->
+			<div class="col-md-8 col-md-pull-4">
+				<div class="footer-copyright">
+					<span>&copy; <script>document.write(new Date().getFullYear());</script> Jtech cert . Here at your service</span>
+				</div>
+			</div>
+			<!-- /copyright -->
+		</div>
+
+	</div>
+</footer>
+
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/easy-responsive-tabs.js"></script>
+<script type="text/javascript" src="assets/js/j.dataTables.min.js"></script>
+<script type="text/javascript" src="assets/js/dataTables.min.js"></script>
+<script type="text/javascript" src="assets/js/main.js"></script>
